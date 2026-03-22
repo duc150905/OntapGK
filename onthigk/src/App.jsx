2c95a787522dc4145529ad33dc3593417c998e89
duc150905 <minhduclephi@gmail.com>
2026-03-22 07:31:30 +0700

    Bai2

--- onthigk/src/App.jsx
+++ onthigk/src/App.jsx
@@ -3,22 +3,39 @@ import './App.css'
 
 function App() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   useEffect(() => {
     const fetchdata = async () => {
-      const res = await fetch("https://jsonplaceholder.typicode.com/users");
-      const data = await res.json();
-      setData(data);
+      setError(null);
+      try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/users");
+        if (!res.ok) throw new Error("API lỗi");
+        const data = await res.json();
+        setData(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
     }
-    fetchdata();
+    setTimeout(() => {
+      fetchdata();
+    }, 2000);
+
   }, [])
   return (
     <>
-      {data.map((item) => (
-        <div key={item.id}>
-          <p>{item.name}</p>
-          <p>{item.email}</p>
-        </div>
-      ))}
+      {loading && <h1>Loading...</h1>}
+      {error && <h1 style={{ color: "red" }}>{error.message}</h1>}
+      {!loading && !error && data && (
+        data.map((item) => (
+          <div key={item.id}>
+            <p>{item.name}</p>
+            <p>{item.email}</p>
+          </div>
+        ))
+      )}
     </>
   )
 }
