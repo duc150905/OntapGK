cb7a71385f720f7f513290c11fd77189fd7b33a4
duc150905 <minhduclephi@gmail.com>
2026-03-22 20:17:07 +0700

    Đọc dữ liệu từ file json

--- onthigk/src/App.jsx
+++ onthigk/src/App.jsx
@@ -1,32 +1,16 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-
+import data from '../data.json';
 function App() {
-  const [search, setSearch] = useState("");
-  const [data, setData] = useState([]);
-  const [filterData, setFilterData] = useState([]);
-  useEffect(() => {
-    const fetchdata = async () => {
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-      const data = await res.json();
-      setData(data);
-    }
-    fetchdata();
-  }, []);
-
-  useEffect(() => {
-    const result = data.filter((data) => data.title.toLowerCase().includes(search.toLowerCase()));
-    setFilterData(result);
-  }, [search]);
   return (
     <div>
-      <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
-      {filterData.map((item) => (
+      {data.map(item => (
         <div key={item.id}>
-          <p>{item.title}</p>
+          <p>{item.name}</p>
         </div>
       ))}
     </div>
+
   )
 }
 
