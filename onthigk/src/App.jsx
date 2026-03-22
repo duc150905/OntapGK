07f7c13eaad8c67719ebd85d696c471b95c92fbc
duc150905 <minhduclephi@gmail.com>
2026-03-22 20:49:27 +0700

    useRef

--- onthigk/src/App.jsx
+++ onthigk/src/App.jsx
@@ -1,16 +1,18 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import './App.css'
-import data from '../data.json';
 function App() {
+  const [count, setCount] = useState(0);
+  const countRef = useRef(0);
   return (
     <div>
-      {data.map(item => (
-        <div key={item.id}>
-          <p>{item.name}</p>
-        </div>
-      ))}
+      <p>state:{count}</p>
+      <p>ref:{countRef.current}</p>
+      <button onClick={() => setCount(count + 1)}>Count+1</button>
+      <button onClick={() => {
+        countRef.current++;
+        console.log(countRef.current);
+      }}>ref+1</button>
     </div>
-
   )
 }
 
